ad542b8391f3311656f53cb71b5c903a229dda72
belkud <vlad-belkud@yandex.ru>
2024-03-08 22:28:48 +0300

    mjh

---
+++ src/components/telephones.vue
@@ -0,0 +1,26 @@
+<template>
+
+</template>
+
+<script setup lang="ts">
+import axios from 'axios';
+import {ref} from 'vue'
+let data = ref()
+const getData  = async function () {
+    try {
+        const get = await axios.get('http://localhost:3001/api/telephones')
+        data.value = get.data
+        console.log(data.value);
+         
+    } catch (error) {
+        
+    }
+} 
+getData()
+</script>
+
+<style>
+
+</style>
+
+
